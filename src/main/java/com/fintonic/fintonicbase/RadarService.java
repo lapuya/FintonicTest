a9b99e0a2cd9be66ad0753a873cb21b4fda55155
Ren <laurence.apuya.p@gmail.com>
2025-03-27 15:56:53 +0100

    Add furthest-enemies implementation and tests

--- src/main/java/com/fintonic/fintonicbase/RadarService.java
+++ src/main/java/com/fintonic/fintonicbase/RadarService.java
@@ -10,12 +10,40 @@ import java.util.List;
 public class RadarService {
 
     public CoordinatesDto processRequest(RequestDto requestDto) {
+        CoordinatesDto coordinatesDto;
+        //aceptamos solo un protocolo por ahora
+        switch (requestDto.protocols().get(0)) {
+            case "closest-enemies":
+                coordinatesDto = calculateClosestEnemy(requestDto);
+                break;
+            case "furthest-enemies":
+                coordinatesDto = calculateFurthestEnemy(requestDto);
+                break;
+            default:
+                coordinatesDto = new CoordinatesDto(0, 0); //excepción, tiramos 0,0 por ahora
+        }
+        return coordinatesDto;
+    }
+
+    private CoordinatesDto calculateClosestEnemy(RequestDto requestDto) {
+
         List<CoordinatesDto> listOfCoordinates = new ArrayList<>();
 
         for (ScanDto scanDto : requestDto.scan()) {
             listOfCoordinates.add(scanDto.coordinates());
         }
         listOfCoordinates.sort(Comparator.comparingInt(RadarService::distance));
+
+        return listOfCoordinates.get(0);
+    }
+
+    private CoordinatesDto calculateFurthestEnemy(RequestDto requestDto) {
+        List<CoordinatesDto> listOfCoordinates = new ArrayList<>();
+
+        for (ScanDto scanDto : requestDto.scan()) {
+            listOfCoordinates.add(scanDto.coordinates());
+        }
+        listOfCoordinates.sort(Comparator.comparingInt(RadarService::distance).reversed());
 
         return listOfCoordinates.get(0);
     }
--- src/test/acceptance/java/com/fintonic/unittesting/RadarServiceTest.java
+++ src/test/acceptance/java/com/fintonic/unittesting/RadarServiceTest.java
@@ -38,7 +38,7 @@ public class RadarServiceTest {
     }
 
     @Test
-    void given_more_than_one_closest_enemy_protocol_closest_enemy_case_1() {
+    void given_more_than_one_enemies_and_protocol_closest_enemy_case_1() {
         List<String> protocols = new ArrayList<>();
         List<ScanDto> scan = new ArrayList<>();
         EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
@@ -57,7 +57,7 @@ public class RadarServiceTest {
     }
 
     @Test
-    void given_more_than_one_closest_enemy_protocol_closest_enemy_case_2() {
+    void given_more_than_one_enemies_and_protocol_closest_enemy_case_2() {
         List<String> protocols = new ArrayList<>();
         List<ScanDto> scan = new ArrayList<>();
         EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
@@ -67,6 +67,63 @@ public class RadarServiceTest {
         ScanDto scanDto4 = new ScanDto(new CoordinatesDto(30, 0), enemiesDto, null);
 
         protocols.add("closest-enemies");
+        scan.add(scanDto1);
+        scan.add(scanDto2);
+        scan.add(scanDto3);
+        scan.add(scanDto4);
+
+        RequestDto requestDto = new RequestDto(protocols, scan);
+
+        var coordinatesDto = radarService.processRequest(requestDto);
+        assertEquals(scanDto3.coordinates(), coordinatesDto);
+    }
+
+    @Test
+    void given_one_furthest_enemy_protocol_return_same_enemy() {
+        List<String> protocols = new ArrayList<>();
+        List<ScanDto> scan = new ArrayList<>();
+        EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
+        ScanDto scanDto = new ScanDto(new CoordinatesDto(0, 40), enemiesDto, null);
+
+        protocols.add("furthest-enemies");
+        scan.add(scanDto);
+
+        RequestDto requestDto = new RequestDto(protocols, scan);
+
+        var coordinatesDto = radarService.processRequest(requestDto);
+        assertEquals(scanDto.coordinates(), coordinatesDto);
+    }
+
+    @Test
+    void given_more_than_one_enemies_and_protocol_furthest_enemy_case_1() {
+        List<String> protocols = new ArrayList<>();
+        List<ScanDto> scan = new ArrayList<>();
+        EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
+        ScanDto scanDto1 = new ScanDto(new CoordinatesDto(0, 40), enemiesDto, null);
+        ScanDto scanDto2 = new ScanDto(new CoordinatesDto(0, 30), enemiesDto, null);
+
+
+        protocols.add("furthest-enemies");
+        scan.add(scanDto1);
+        scan.add(scanDto2);
+
+        RequestDto requestDto = new RequestDto(protocols, scan);
+
+        var coordinatesDto = radarService.processRequest(requestDto);
+        assertEquals(scanDto1.coordinates(), coordinatesDto);
+    }
+
+    @Test
+    void given_more_than_one_enemies_and_protocol_furthest_enemy_case_2() {
+        List<String> protocols = new ArrayList<>();
+        List<ScanDto> scan = new ArrayList<>();
+        EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
+        ScanDto scanDto1 = new ScanDto(new CoordinatesDto(41, 3), enemiesDto, null);
+        ScanDto scanDto2 = new ScanDto(new CoordinatesDto(0, 30), enemiesDto, null);
+        ScanDto scanDto3 = new ScanDto(new CoordinatesDto(30, 40), enemiesDto, null);
+        ScanDto scanDto4 = new ScanDto(new CoordinatesDto(30, 0), enemiesDto, null);
+
+        protocols.add("furthest-enemies");
         scan.add(scanDto1);
         scan.add(scanDto2);
         scan.add(scanDto3);
