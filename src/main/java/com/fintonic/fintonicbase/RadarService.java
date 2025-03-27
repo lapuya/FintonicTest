c31273383ab63d0fe991e4e5fa4c1fe0b06b0717
Ren <laurence.apuya.p@gmail.com>
2025-03-27 15:42:47 +0100

    Implement closest enemies, add 1 more test

--- src/main/java/com/fintonic/fintonicbase/RadarService.java
+++ src/main/java/com/fintonic/fintonicbase/RadarService.java
@@ -2,10 +2,25 @@ package com.fintonic.fintonicbase;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 @Service
 public class RadarService {
 
     public CoordinatesDto processRequest(RequestDto requestDto) {
-        return new CoordinatesDto(0, 0);
+        List<CoordinatesDto> listOfCoordinates = new ArrayList<>();
+
+        for (ScanDto scanDto : requestDto.scan()) {
+            listOfCoordinates.add(scanDto.coordinates());
+        }
+        listOfCoordinates.sort(Comparator.comparingInt(RadarService::distance));
+
+        return listOfCoordinates.get(0);
+    }
+
+    private static int distance(CoordinatesDto coordinates) {
+        return (int) Math.sqrt(coordinates.x() * coordinates.x() + coordinates.y() * coordinates.y());
     }
 }
--- src/test/acceptance/java/com/fintonic/unittesting/RadarServiceTest.java
+++ src/test/acceptance/java/com/fintonic/unittesting/RadarServiceTest.java
@@ -4,16 +4,12 @@ import com.fintonic.fintonicbase.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.http.HttpStatus;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
 
 public class RadarServiceTest {
 
@@ -42,7 +38,7 @@ public class RadarServiceTest {
     }
 
     @Test
-    void given_more_than_one_closest_enemy_protocol_closest_enemy() {
+    void given_more_than_one_closest_enemy_protocol_closest_enemy_case_1() {
         List<String> protocols = new ArrayList<>();
         List<ScanDto> scan = new ArrayList<>();
         EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
@@ -58,5 +54,27 @@ public class RadarServiceTest {
 
         var coordinatesDto = radarService.processRequest(requestDto);
         assertEquals(scanDto2.coordinates(), coordinatesDto);
+    }
+
+    @Test
+    void given_more_than_one_closest_enemy_protocol_closest_enemy_case_2() {
+        List<String> protocols = new ArrayList<>();
+        List<ScanDto> scan = new ArrayList<>();
+        EnemiesDto enemiesDto = new EnemiesDto(EnemiesType.SOLDIER, 10);
+        ScanDto scanDto1 = new ScanDto(new CoordinatesDto(41, 3), enemiesDto, null);
+        ScanDto scanDto2 = new ScanDto(new CoordinatesDto(0, 30), enemiesDto, null);
+        ScanDto scanDto3 = new ScanDto(new CoordinatesDto(2, 3), enemiesDto, null);
+        ScanDto scanDto4 = new ScanDto(new CoordinatesDto(30, 0), enemiesDto, null);
+
+        protocols.add("closest-enemies");
+        scan.add(scanDto1);
+        scan.add(scanDto2);
+        scan.add(scanDto3);
+        scan.add(scanDto4);
+
+        RequestDto requestDto = new RequestDto(protocols, scan);
+
+        var coordinatesDto = radarService.processRequest(requestDto);
+        assertEquals(scanDto3.coordinates(), coordinatesDto);
     }
 }
